1743cb4a51c7d0d7f21d8c01aef56afbc9470f2c
root <luann4099@gmail.com>
2020-01-09 20:24:30 +0700

    bai 7 update 3

--- bai7/slide.js
+++ bai7/slide.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     var index = $('.slider ul li').length - 1;
     $('.btn-next').click(function () {
-        $('.slider ul li').eq(index).hide();
-        index--;
+        if (index > 0) {
+            $('.slider ul li').eq(index).fadeOut();
+            index--;
+        }
     });
 
     $('.btn-prev').click(function () {
-        index++;
-        $('.slider ul li').eq(index).show();
+        if (index < ($('.slider ul li').length - 1)) {
+            index++;
+            $('.slider ul li').eq(index).fadeIn();
+        }
     })
 })
